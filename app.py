f3bc4a62176a9cfc78446e90b1535ef3a57f9934
Zviad1990 <munchia@mail.ru>
2020-06-18 18:59:59 +0300

    пробный запуск

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask_ngrok import run_with_ngrok
+from flask import Flask, request, jsonify
+import pandas as pd
+
+
+# Пробный запуск Flask
+
+app = Flask(__name__)
+run_with_ngrok(app)  # Start ngrok when app is run
+
+@app.route("/a")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
